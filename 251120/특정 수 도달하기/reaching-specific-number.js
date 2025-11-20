331d7867647cb158845cde630f1deef089c7a492
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-11-20 19:32:17 +0900

    Title: 특정 수 도달하기, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251120/특정 수 도달하기/reaching-specific-number.js
+++ 251120/특정 수 도달하기/reaching-specific-number.js
@@ -2,13 +2,15 @@ const fs = require("fs");
 let arr = fs.readFileSync(0).toString().trim().split(" ");
 
 let add = 0;
+let slash = 0;
 
 for (let a of arr){
     let b = Number(a);
     if (b > 250) break;
     add += b;
+    slash += 1;
 }
 
-let av = (add / 5).toFixed(1);
+let av = (add / slash).toFixed(1);
 
 console.log(add + " " + av);
