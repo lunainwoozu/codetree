448a951fc6ac1ff485f880af8086db6fdd0f7502
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-15 19:41:45 +0900

    Title: 중앙값 구하기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251215/중앙값 구하기/find-the-median.js
+++ 251215/중앙값 구하기/find-the-median.js
@@ -5,22 +5,20 @@ let a = parseInt(input[0]);
 let b = parseInt(input[1]);
 let c = parseInt(input[2]);
 
-if (a > b && a > c){
+if (a > b){
     if (b > c){
         console.log(b);
-    } else {
+    } else if (a > c) {
         console.log(c);
-    }
-} else if (a > b) {
-    if (c > a){
+    } else {
         console.log(a);
-    } else {
-        console.log(c);
     }
 } else {
-    if (c > b){
-        console.log(b)
-    } else {
+    if (a > c){
+        console.log(a)
+    } else if (b > c) {
         console.log(c)
+    } else {
+        console.log(b)
     }
 }
