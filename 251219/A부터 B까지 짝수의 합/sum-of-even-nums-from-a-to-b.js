0c2b24c2fbb4d204a9ab0ce54be3ef8fdf8cde44
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-19 23:54:57 +0900

    Title: A부터 B까지 짝수의 합, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251219/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let A = Number(input[0]);
+let B = Number(input[1]);
+let n = 0;
+
+for (let i = A; i < B; i++){
+    if (i % 2 === 0){
+        n += i;
+    }
+}
+
+console.log(n)
