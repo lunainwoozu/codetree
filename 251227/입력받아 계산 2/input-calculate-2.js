121c18b2d1d4d906c48379bd6ecf9399b30e464f
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-27 14:30:07 +0900

    Title: 입력받아 계산 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251227/입력받아 계산 2/input-calculate-2.js
+++ 251227/입력받아 계산 2/input-calculate-2.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString.trim().split(' ');
+const input = fs.readFileSync(0).toString().trim().split(' ');
 
 const a = Number(input[0]);
 const b = Number(input[1]);
