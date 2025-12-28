1feeccc8d3fbab7335e324823aeb05a2e1912740
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-28 14:09:54 +0900

    Title: 두수의 곱과 몫, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251228/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ');
+
+let [a, b] = input.map(Number);
+
+console.log(a + " * " + b + " = " + a * b);
+console.log(a + " / " + b + " = " + Math.floor(a / b));
