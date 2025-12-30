5710b2476761b3d8b41bf96142616fec3d68e7f6
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-30 17:04:29 +0900

    Title: 두 수의 짝홀 여부, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251230/두 수의 짝홀 여부/parity-of-two-numbers.js
+++ 251230/두 수의 짝홀 여부/parity-of-two-numbers.js
@@ -4,11 +4,11 @@ const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 if (a % 2 === 0){
     console.log('even');
 } else {
-    console.lot('odd');
+    console.log('odd');
 }
 
 if (b % 2 === 0){
     console.log('even');
 } else {
-    console.lot('odd');
+    console.log('odd');
 }
