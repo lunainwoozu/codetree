cd9cffbbfa78e3f196399842ef408ca21d1bc6aa
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-30 16:44:07 +0900

    Title: 등급 매기기, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251230/등급 매기기/ranking.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const N = Number(fs.readFileSync(0));
+
+if (N >= 90){
+    console.log('A');
+} else if (N >= 80){
+    console.log('B');
+} else if (N >= 70){
+    console.log('C');
+} else if (N >= 60){
+    console.log('D');
+} else {
+    console.log('F');
+}
