2d4c04c58badd73073e019dff9d93e7c5516a46a
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-30 17:16:19 +0900

    Title: 4번의 크기 비교, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 251230/4번의 크기 비교/4-time-comparison.js
+++ 251230/4번의 크기 비교/4-time-comparison.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const A = Number(input[0]);
-const [B, C, D, E] = input[1].map(Number);
+const [B, C, D, E] = input[1].split(' ').map(Number);
 
 console.log(A > B ? 1 : 0);
 console.log(A > C ? 1 : 0);
