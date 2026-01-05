37cc02f735be18021b07c550459bff01b57ae888
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-05 15:49:41 +0900

    Title: 3의 배수 출력하기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 260105/3의 배수 출력하기/print-multiple-of-number-three.js
+++ 260105/3의 배수 출력하기/print-multiple-of-number-three.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0));
 
-let i = 1;
+let i = 3;
 
 while (i <= n){
-    if(i % 3 === 0){
-        process.stdout.write(i + ' ');
-    }
-    i++;
+    process.stdout.write(i + ' ');
+    i += 3;
 }
