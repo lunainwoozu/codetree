18a2425d5457db88a2db9008d1c34b4472a990d9
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-16 19:51:18 +0900

    Title: 2의 거듭제곱수, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260116/2의 거듭제곱수/pow-of-2.js
+++ 260116/2의 거듭제곱수/pow-of-2.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
-let n = Number(fs.readFileSync(0));
+const n = Number(fs.readFileSync(0));
 
 let i = 1;
 let cnt = 0;
 
-while (n > 1) {
-    n = Math.floor(n / 2);
-    i++;
-    cnt++;
+while (true) {
+    if (n === 2 ** i) {
+        console.log(cnt);
+        break;
+    } else {
+        cnt++;
+        i++;
+    }
 }
-
-console.log(cnt);
