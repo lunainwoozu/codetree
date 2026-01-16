12e1f1bebb5e405c4851cdcb67572ffa9b563a03
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-16 20:05:04 +0900

    Title: 조건에 따른 연산, Time: 61ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260116/조건에 따른 연산/operatino-by-rule.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0));
+let cnt = 0;
+
+while (n < 1000) {
+    cnt++;
+    if (n % 2 === 0) {
+        n = n * 3 + 1;
+    } else {
+        n = n * 2 + 2;
+    }
+}
+
+console.log(cnt);
