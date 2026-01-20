45b3bb7c2d3275180fc6c9c0118fe2295659f7e5
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-20 18:00:06 +0900

    Title: 몫 출력하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260120/몫 출력하기/print-share.js
+++ 260120/몫 출력하기/print-share.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 
 let i = 0;
+let cnt = 0;
 
 while (true) {
     let n = input[i];
-    if (n % 2 === 0) {
-        console.log(Math.floor(n / 2));
-    }
-
     i++;
 
-    if (i === 4) break;
+    if (n % 2 === 0) {
+        console.log(Math.floor(n / 2));
+        cnt++;
+    }
+
+    if (cnt === 3) break;
 }
