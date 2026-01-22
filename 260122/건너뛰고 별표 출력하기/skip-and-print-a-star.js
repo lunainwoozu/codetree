11810e1c78afbd5678aeb89fcba15f27e5a0a166
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-22 17:39:33 +0900

    Title: 건너뛰고 별표 출력하기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260122/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0));
+let cnt = 0;
+
+for (let i = 0; i < n * 2 - 1; i++) {
+    str = '';
+    if (i < n) {
+        cnt++;
+    } else {
+        cnt--;
+    }
+    for (let j = 0; j < cnt; j++) {
+        str += '*';
+    }
+    console.log(str);
+    console.log('');
+}
