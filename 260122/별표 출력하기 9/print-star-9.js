faeca2874a1f80b51ec49880367c15b8750ef91f
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-22 18:18:49 +0900

    Title: 별표 출력하기 9, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260122/별표 출력하기 9/print-star-9.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0));
+
+for (let i = 0; i < n; i++) {
+    str = '';
+    for (let j = 0; j < n - i - 1; j++) {
+        str += '  ';
+    }
+    for (let j = 0; j < i * 2 + 1; j++) {
+        str += '* ';
+    }
+    console.log(str);
+}
