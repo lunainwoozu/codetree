b81042061f034648305a42dbcd0ffb1b0f63504a
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-25 23:30:42 +0900

    Title: 별표 출력하기 12, Time: 73ms, Memory: 11MB, Status: Runtime Error - Codetree

---
+++ 260125/별표 출력하기 12/print-star-12.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0));
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j; j++) {
+        if (i === 0) {
+            process.stdout.write("* ");
+        }
+        if (j % 2 !== 0) {
+            for (let k = n; k < ; k++) {}
+        }
+    }
+    console.log();
+}
