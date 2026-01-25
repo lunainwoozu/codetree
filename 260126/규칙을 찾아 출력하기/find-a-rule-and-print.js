4b606b32943dca2c337be08c0138f7931020d64e
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-26 00:43:50 +0900

    Title: 규칙을 찾아 출력하기, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260126/규칙을 찾아 출력하기/find-a-rule-and-print.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0));
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        if (i === 0 || j === 0 || i === n - 1 || j === n - 1) {
+            str += "* ";
+        } else {
+            if (j <= i - 1) {
+                str += "* ";
+            } else {
+                str += "  ";
+            }
+        }
+    }
+    console.log(str);
+}
